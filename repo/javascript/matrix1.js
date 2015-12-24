16248c2c08e285fb209ac6f1a4d8ccb07354df97
Johan Rivera <yohanate@gmail.com>
2015-12-24 14:12:31 +0800

    adjusted js file

--- repo/javascript/matrix1.js
+++ repo/javascript/matrix1.js
@@ -10,7 +10,7 @@ function applyChanges() {
         }
     }
     document.getElementById('jenkins-head-icon').src = 'http://yohanate.github.io/matrixcss/repo/img/jenkins-head-icon-small.png';
-    document.getElementById('jenkins-head-icon').show();
+    document.getElementById('jenkins-head-icon').style.display = 'block';
 };
 var body = document.getElementsByTagName('body');
 if (window.addEventListener) {
