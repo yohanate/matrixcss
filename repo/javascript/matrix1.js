86198a04b14e4557a0a04c7a093938f73395772e
Johan Rivera <yohanate@gmail.com>
2015-12-24 14:07:52 +0800

    adjusted js file

--- repo/javascript/matrix1.js
+++ repo/javascript/matrix1.js
@@ -13,10 +13,10 @@ function myFunction() {
 var body = document.getElementsByTagName('body');
 if (window.addEventListener) {
     window.addEventListener('load', myFunction, false);
+    document.getElementById('jenkins-head-icon').show();
+    document.getElementById('jenkins-head-icon').src = 'http://yohanate.github.io/matrixcss/repo/img/jenkins-head-icon-small.png';
 }
 else if (window.attachEvent) {
     window.attachEvent('onload', myFunction );
-    document.getElementById('jenkins-head-icon').show();
-    document.getElementById('jenkins-head-icon').src = 'http://yohanate.github.io/matrixcss/repo/img/jenkins-head-icon-small.png';
 }
 
